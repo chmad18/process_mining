941a20cbbc30cb021df89581365d94df41798296
Solvi Palsson <s174391@student.dtu.dk>
2023-11-26 18:45:44 +0100

    Data Dimension Checks
    NormalDistributionCheck and shapiro test, ChiSquared and KDE

--- DataDimension/main.py
+++ DataDimension/main.py
@@ -1,92 +1,128 @@
 from filehandler import read_from_file, files_dictionary
-from datadimension import get_statistics_per_trace, get_kde_models, get_activity_stats
-import seaborn as sns
-import matplotlib.pyplot as plt
-from scipy.stats import poisson
-import numpy as np
-from scipy.stats import chisquare
+from datadimension import get_statistics_per_trace, get_frequency_fitness, get_duration_fitness_kde, get_duration_fitness_ChiSquared, get_frequency_fitness_kde
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import statsmodels.api as sm
+import os
+my_path = os.getcwd()
 
 files_dictionary = files_dictionary()
 
 frequency_df = pd.DataFrame()
+kde_duration_df = pd.DataFrame()
+chi_duration_df = pd.DataFrame()
+kde_frequency_df = pd.DataFrame()
 
 for scenario in files_dictionary:
     log_file_normal = files_dictionary[scenario]["normal"][0]
     log_normal = read_from_file(log_file_normal)
     statistics_df = get_statistics_per_trace(log_normal)
 
-    # Get a list of all unique activities
+    # Get a list of all unique activities in the reference log
     all_activities = statistics_df['Activity'].unique()
-    # Group by TraceID and Activity, then count the frequency of each activity per trace
-    activity_frequency_per_trace = statistics_df.groupby(['TraceID', 'Activity']).size().unstack(fill_value=0)
-    # Fill in zeros for activities not mentioned in each trace
-    activity_frequency_filled = activity_frequency_per_trace.reindex(columns=all_activities, fill_value=0)
 
-    # Calculating the frequency of each activity per trace
-    frequency_per_trace = statistics_df.groupby(['TraceID', 'Activity']).size().reset_index(name='Frequency')
+    #Duration
+    duration_stats = statistics_df.groupby(['TraceID', 'Activity'])['Duration'].sum().unstack(fill_value=0).reindex(columns=all_activities, fill_value=0)
+    aggregated_duration_stats = duration_stats.agg(['mean', 'std', 'min', 'max', 'median'])
+    expected_durations = aggregated_duration_stats.loc['mean']
+    
+    #Frequency
+    freq_stats = statistics_df.groupby(['TraceID', 'Activity']).size().unstack(fill_value=0).reindex(columns=all_activities, fill_value=0)
+    aggregated_freq_stats = freq_stats.agg(['mean', 'std', 'min', 'max', 'median'])
+    expected_frequencies = aggregated_freq_stats.loc['mean']
 
-    # Calculate aggregated statistics
-    aggregated_stats = activity_frequency_filled.agg(['mean', 'std', 'min', 'max', 'median'])
+    #Check Normal Distribution Assumption. Saves Histogram to the specified folders. The plot contains the P value and if the normal distribution is accepted or not
+    alpha = 0.05
+    logName = log_file_normal.removesuffix('.xes').replace('/', '\\')
+    for activity in all_activities:
+        s = ''
+        r = ''
+        statfreq, pfreq = stats.shapiro(freq_stats[activity])
+        pfreq=float(format(pfreq, '.2f'))
+        if pfreq > alpha:
+            s = 'P Value : ' + str(pfreq) + ' Sample looks Gaussian (fail to reject H0)'
+        else:
+            s = 'P Value : ' + str(pfreq) + ' Sample does not look Gaussian (reject H0)'
 
-    # Extract mean frequencies as expected frequencies
-    expected_frequencies = aggregated_stats.loc['mean']
+        plt.hist(freq_stats[activity], bins=20, alpha=0.7, color='blue', edgecolor='black')
+        plt.text(-5,-5, s, fontsize = 12) 
+        plt.title('Frequency : ' + activity + ' ' + logName)
+        freqfilename = 'DataDimension\\Results\\NormalDistributionChecks\\' + logName + '_' + activity + '_freq.png'
+        plt.savefig(os.path.join(my_path, freqfilename))
+        plt.close()
+
+        statdur, pdur = stats.shapiro(freq_stats[activity])
+        pdur=float(format(pdur, '.2f'))
+        if pdur > alpha:
+            r = 'P Value : ' + str(pdur) + ' Sample looks Gaussian (fail to reject H0)'
+        else:
+            r = 'P Value : ' + str(pdur) + ' Sample does not look Gaussian (reject H0)'
+
+        plt.hist(duration_stats[activity], bins=20, alpha=0.7, color='red', edgecolor='black', label=r)
+        plt.title('Duration : ' + activity + ' ' + logName)
+        plt.text(-5, -5, r, fontsize = 12) 
+        durfilename = 'DataDimension\\Results\\NormalDistributionChecks\\'+ logName + '_' + activity + '_duration.png'
+        plt.savefig(os.path.join(my_path, durfilename))
+        plt.close()
+
 
     for log_file_enriched in files_dictionary[scenario]["enriched"]:
         log_enriched = read_from_file(log_file_enriched)
         statistics_enriched_df = get_statistics_per_trace(log_enriched)
-        # Group by TraceID and Activity, then count the frequency of each activity per trace
-        test_activity_frequency_per_trace = statistics_enriched_df.groupby(['TraceID', 'Activity']).size().unstack(fill_value=0)
-        # Fill in zeros for activities not mentioned in each trace
-        test_activity_frequency_filled = test_activity_frequency_per_trace.reindex(columns=all_activities, fill_value=0)
 
-        p_values = []
-        chi2_stat_values = []
+        #Duration - KDE
+        duration_fitness_value = get_duration_fitness_kde(statistics_enriched_df, duration_stats, all_activities)
+        duration_row = pd.DataFrame([
+            {'Scenario' : scenario,
+            'Normal Log': log_file_normal,
+            'Enriched Log': log_file_enriched,
+            'Fitness Value': duration_fitness_value
+            }])
 
-        for trace_id, trace_data in test_activity_frequency_filled.iterrows():
-            trace_observed_frequencies = []
-            trace_expected_frequencies = []
-            for activity in all_activities:
-                freq = trace_data.get(activity, 0)
-                trace_observed_frequencies.append(freq)
+        kde_duration_df = pd.concat([kde_duration_df, duration_row], ignore_index=True)
 
+        #Frequency - KDE
+        freq_fitness_value_kde = get_frequency_fitness_kde(statistics_enriched_df, freq_stats, all_activities)
+        freq_row_kde = pd.DataFrame([
+            {'Scenario' : scenario,
+            'Normal Log': log_file_normal,
+            'Enriched Log': log_file_enriched,
+            'Fitness Value': freq_fitness_value_kde
+            }])
 
-                expected_freq = expected_frequencies.get(activity)
-                trace_expected_frequencies.append(expected_freq)
+        kde_frequency_df = pd.concat([kde_frequency_df, freq_row_kde], ignore_index=True)
 
-            # Convert to numpy arrays to ensure sums match
-            trace_observed_frequencies = np.array(trace_observed_frequencies)
-            trace_expected_frequencies = np.array(trace_expected_frequencies)
+        #Duration - Chi-Squared
+        chi2_stat_dur, p_value_chi_dur, fitness_value_chi_dur = get_duration_fitness_ChiSquared(statistics_enriched_df, expected_durations, all_activities)
 
-            if trace_observed_frequencies.sum() > 0:
-                scale_factor = trace_observed_frequencies.sum() / trace_expected_frequencies.sum()
-                scaled_expected_frequencies = trace_expected_frequencies * scale_factor
+        duration_row_chi = pd.DataFrame([
+            {'Scenario' : scenario,
+            'Normal Log': log_file_normal,
+            'Enriched Log': log_file_enriched,
+            'Fitness Value': fitness_value_chi_dur,
+            'P Value' : p_value_chi_dur,
+            'Chi Stat' : chi2_stat_dur}])
+        
+        chi_duration_df = pd.concat([chi_duration_df, duration_row_chi], ignore_index=True)
 
-                # Perform Chi-Squared Test
-                chi2_stat, p_value = chisquare(f_obs=trace_observed_frequencies, f_exp=scaled_expected_frequencies)
+        
+        
+        #Frequency - Chi-Squared
+        chi2_stat, p_value, fitness_value = get_frequency_fitness(statistics_enriched_df, expected_frequencies, all_activities)
 
-                normalized_chi2_stat = chi2_stat / sum(trace_expected_frequencies)
-                chi2_stat_values.append(normalized_chi2_stat)
-                p_values.append(p_value)
-                # Store or use the chi2_stat and p_value as needed
-            else:
-                chi2_stat_values.append(0)
-                p_values.append(0)
-
-        chi2_stat_average = sum(chi2_stat_values) / len(chi2_stat_values)
-        p_value_average = sum(p_values) / len(p_values)
-
-        fitness_value = (1 - chi2_stat_average) * p_value_average
-
-        new_row_df = pd.DataFrame([
+        frequency_row = pd.DataFrame([
             {'Scenario' : scenario,
             'Normal Log': log_file_normal,
             'Enriched Log': log_file_enriched,
             'Fitness Value': fitness_value,
-            'P Value' : p_value_average,
-            'Chi Stat' : chi2_stat_average}])
+            'P Value' : p_value,
+            'Chi Stat' : chi2_stat}])
         
-        frequency_df = pd.concat([frequency_df, new_row_df], ignore_index=True)
+        frequency_df = pd.concat([frequency_df, frequency_row], ignore_index=True)
 
-frequency_df.to_excel('DataDimension/Results/frequency_results.xlsx')
+
+chi_duration_df.to_excel('DataDimension/Results/ChiSquared/duration_results.xlsx')
+frequency_df.to_excel('DataDimension/Results/ChiSquared/frequency_results.xlsx')
+kde_duration_df.to_excel('DataDimension/Results/KDE/duration_results.xlsx')
+kde_frequency_df.to_excel('DataDimension/Results/KDE/frequency_results.xlsx')
